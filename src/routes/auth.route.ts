82360ac57ff75c331d81f389b75060bcc26a51e0
SharanSapkota <ram@noveltytechnology.com>
2024-03-21 07:35:28 +0545

    Comment unwanted code

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -4,13 +4,13 @@ import { UserController } from '../controllers/user.controller';
 const authRouter = Router();
 const userController = new UserController();
 
-authRouter.get('/login', (req: any, res) => userController.login(req, res))
-authRouter.post('/signup', (req, res) => userController.signup(req, res))
-authRouter.get('/getallusers', (req, res) => userController.getAllUsers(req, res))
+// authRouter.get('/login', (req: any, res) => userController.login(req, res))
+// authRouter.post('/signup', (req, res) => userController.signup(req, res))
+// authRouter.get('/getallusers', (req, res) => userController.getAllUsers(req, res))
 
-// This defines roles route
-authRouter.post('/role', (req, res) => userController.createRole(req, res))
-authRouter.get('/role', (req, res) => userController.getAllRoles(req, res))
+// // This defines roles route
+// authRouter.post('/role', (req, res) => userController.createRole(req, res))
+// authRouter.get('/role', (req, res) => userController.getAllRoles(req, res))
 
 export = authRouter;
 
