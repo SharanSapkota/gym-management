26c04b52a1cd93004aaac46d5e5385a8319ad6f7
SharanSapkota <sapkotarambbo@gmail.com>
2024-03-22 20:36:05 +0545

    Add validation middlware

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,12 +1,12 @@
 import {Request, Router} from 'express';
 import { UserController } from '../controllers/user.controller';
 import { authenticate } from '../middleware/auth.middleware';
+import { validateUser } from '../middleware/validation.middleware';
 
 const userRouter = Router();
 const userController = new UserController();
 
-
-userRouter.post('/', authenticate, userController.create)
+userRouter.post('/', authenticate, validateUser, userController.create)
 userRouter.get('/', authenticate, userController.findAll)
 
 export = userRouter;
