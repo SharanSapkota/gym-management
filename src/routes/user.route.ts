ea7f1fa9494a4762950ee64842f06986cd889845
SharanSapkota <sapkotarambbo@gmail.com>
2024-03-22 21:23:59 +0545

    Add login route

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,12 +1,15 @@
 import {Request, Router} from 'express';
 import { UserController } from '../controllers/user.controller';
 import { authenticate } from '../middleware/auth.middleware';
-import { validateUser } from '../middleware/validation.middleware';
+import { validateUserSignup } from '../middleware/validation.middleware';
 
 const userRouter = Router();
 const userController = new UserController();
 
-userRouter.post('/', authenticate, validateUser, userController.create)
+//LOGIN
+userRouter.post('/login', userController.login)
+
+userRouter.post('/', authenticate, validateUserSignup, userController.create)
 userRouter.get('/', authenticate, userController.findAll)
 
 export = userRouter;
