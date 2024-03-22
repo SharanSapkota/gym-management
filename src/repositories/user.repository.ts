75d7fd89abcadc3196cde18a36054fa1eaf04a61
SharanSapkota <sapkotarambbo@gmail.com>
2024-03-22 21:24:11 +0545

    Add login repository

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,5 +1,7 @@
+import { passwordMatch } from "../adapters/encrytion";
+import httpResponse from "../helper/httpResponse";
 import { User } from "../models/user.schema";
-import { BaseRepository } from "./base.repository"
+import { BaseRepository } from "./base.repository";
 
 export class userRepository extends BaseRepository {
     model: any
@@ -10,5 +12,11 @@ export class userRepository extends BaseRepository {
 
     async findOne(payload: any) {
         console.log('Method not implemented (Repository)!')
+    }
+
+    async login(payload: any) {
+        const user = await this.model.findOne({email: payload.email});
+        const isPasswordMatch = passwordMatch(payload.password, user.password)
+        return isPasswordMatch
     }
 }
