a844fe3b6508f61e0aafd0e960acc1134a7c51e8
SharanSapkota <sapkotarambbo@gmail.com>
2024-03-23 08:14:00 +0545

    install zod error

--- src/middleware/validation.middleware.ts
+++ src/middleware/validation.middleware.ts
@@ -1,19 +1,22 @@
 import { NextFunction, Request, Response } from 'express';
 import { z } from 'zod';
+import { fromZodError } from 'zod-validation-error';
+import httpResponse from '../helper/httpResponse';
+
 
 const userSchema = z.object({
   username: z.string().min(1).max(255),
   email: z.string().email(),
   password: z.string().min(6),
   fullName: z.string().min(1).max(255),
-  dateOfBirth: z.date().optional(),
+  dateOfBirth: z.string().optional(),
   gender: z.enum(['Male', 'Female', 'Other']).optional(),
   address: z.string().optional(),
   phoneNumber: z.string().optional(),
   membershipType: z.string().optional(),
-  membershipStartDate: z.date().optional(),
-  membershipEndDate: z.date().optional(),
-  role: z.string().uuid(), 
+  membershipStartDate: z.string().optional(),
+  membershipEndDate: z.string().optional(),
+  role: z.string().optional(), 
   isActiveMember: z.boolean().optional(),
   isUser: z.boolean().optional(),
 });
@@ -23,8 +26,11 @@ function validateUserSignup(req: Request, res: Response, next: NextFunction) {
     userSchema.parse(req.body);
     next();
   } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
+    const errorFormat = {
+        message: fromZodError(error).toString()
+      }
+      
+      httpResponse.errorHandler(res, errorFormat)  }
 }
 
 export { validateUserSignup };
